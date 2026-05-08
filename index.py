5463b21d72fbd1df13b76ba5d07eb9cbf28f5f29
mostakinahmed <mostakin4556@gmail.com>
2026-05-08 18:07:04 +0600

    first

---
+++ index.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+data = [[4, 3, 2], [8, 7, 5], [5, 8, 7]]
+arr = np.array(data)
+
+print(arr)
+
+
+print(arr[1,0])
---
+++ range.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+
+arr = np.array(range(1,5)).reshape(2,2)
+print(arr)
+
+
+print(arr.ndim)
