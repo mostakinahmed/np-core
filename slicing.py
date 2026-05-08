42e56e9a48a4d787fee1aa9c08e4f7f89aa6ea8d
mostakinahmed <mostakin4556@gmail.com>
2026-05-08 18:21:21 +0600

    first

--- slicing.py
+++ slicing.py
@@ -10,3 +10,10 @@ print(arr[2:6:2])
 print(arr[::2])
 
 print(arr[::-1])
+
+print("\n\n")
+arr2  =  np.array([[8,4,6,1,3,5],[8,1,6,7,3,2],[8,4,3,1,4,2],[8,7,3,9,2,2]])
+print(arr2)
+print("\n\n")
+print(arr2[1:4,2:6])
+
